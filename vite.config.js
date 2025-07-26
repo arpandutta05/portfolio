144294cd7881d9d6a7e713cd5f1b9c37b322f116
ARPAN <arpandutta.in@gmail.com>
2025-07-26 13:27:12 +0530

    fix: set correct output dir for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   build: {
-  outDir: 'dist',
-},
-
+    outDir: 'dist', // <- This is the fix
+  },
 });
